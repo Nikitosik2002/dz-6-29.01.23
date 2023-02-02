5722a9ddf0807269ea53e532ddc86eaa76a9f08e
Nikitosik <nikita.kopeickin@yandex.ru>
2023-02-03 00:31:56 +0300

    summ

---
+++ summDiagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿System.Console.WriteLine("Введите количество строк матрицы");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов матрицы");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m,n];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(1,10);
+        Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int summDiagonal = 0;
+
+ for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i == j)
+        {
+            summDiagonal += matrix[i,j];
+        }
+    }
+    
+}
+
+System.Console.WriteLine($"Сумма главной диагонали = {summDiagonal}");
