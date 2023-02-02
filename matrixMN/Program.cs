1161f3e2bdbb521dd3345d24ffcb50b49a0f6c5a
Nikitosik <nikita.kopeickin@yandex.ru>
2023-02-03 00:24:22 +0300

    print matrix M+N

---
+++ matrixMN/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите количество строк матрицы");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов матрицы");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m,n];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = i + j;
+        Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
